9a217ce5727c85a01f0422bdff6da2aeba01dcad
zuweie <jojoe.wei@gmail.com>
2020-02-27 12:20:55 +0800

    implement backend console

--- backend-console/index.js
+++ backend-console/index.js
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2019-12-09 15:48:15
- * @LastEditTime: 2020-02-27 11:59:04
+ * @LastEditTime: 2020-02-27 12:17:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /egg-mini-admin/index.js
@@ -21,12 +21,14 @@ try {
         const install = require('./install');
         install();
     }else if (action == 'login') {
-        console.log(colors.green('login --login=account --password=pwd'));
+        console.log(colors.yellow('usage: login <account (default admin)> <password (default 123456)>'));
+        let acc = process.argv[3]? process.argv[3] : 'admin';
+        let pwd = process.argv[4]? process.args[4] : '123456';
         const login = require('./login');
-        login();
+        login(acc, pwd);
 
     }else if (action == 'create:bucket') {
-        console.log(colors.green('create bucket ~ '));
+        console.log(colors.yellow('usage: bucket:create <bucket name(required)>'));
         //console.log(process.argv[3]);
         let bucket = process.argv[3];
         if (bucket) {
@@ -40,6 +42,7 @@ try {
         const showBucket = require('./showbucket');
         showBucket();
     }else if (action == 'delete:bucket') {
+        console.log(colors.yellow('usage: bucket:delete <bucket id(required)>'))
         let bucket_id = process.argv[3];
         if (bucket_id) {
             const deleteBucket = require('./deletebucket');
